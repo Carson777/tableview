3afc2fe4b32916d0d8677183a204c548863501df
Carson777 <carsoncapper@gmail.com>
2016-06-22 15:23:28 -0500

    TableView
    Week 2 Day 2 Assignment

---
+++ tableview/CustomerTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  CustomerTableViewController.h
+//  tableview
+//
+//  Created by Carson Capper on 6/21/16.
+//  Copyright © 2016 Carson Capper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomerTableViewController : UITableViewController
+
+{
+    NSMutableArray *_customers;
+}
+
+@end
